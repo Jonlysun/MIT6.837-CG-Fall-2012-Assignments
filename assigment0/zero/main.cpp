8eeee2173bfbf729636818d4a5a8575bd0eaa561
jonlysun <2193813848@qq.com>
2022-02-18 13:35:16 +0800

    assignment0

---
+++ assigment0/zero/main.cpp
@@ -0,0 +1,277 @@
+#include "GL/freeglut.h"
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include "vecmath.h"
+using namespace std;
+
+// Globals
+int color_num = 0;
+float light_lp = 1.0;
+float light_rp = 1.0;
+const int buffer_size = 4096;
+
+// This is the list of points (3D vectors)
+vector<Vector3f> vecv;
+
+// This is the list of normals (also 3D vectors)
+vector<Vector3f> vecn;
+
+// This is the list of faces (indices into vecv and vecn)
+vector<vector<int>> vecf;
+
+// You will need more global variables to implement color and position changes
+
+// These are convenience functions which allow us to call OpenGL 
+// methods on Vec3d objects
+inline void glVertex(const Vector3f &a) 
+{ glVertex3fv(a); }
+
+inline void glNormal(const Vector3f &a) 
+{ glNormal3fv(a); }
+
+
+// This function is called whenever a "Normal" key press is received.
+void keyboardFunc( unsigned char key, int x, int y )
+{
+    switch ( key )
+    {
+    case 27: // Escape key
+        exit(0);
+        break;
+    case 'c':
+        // add code to change color here
+		
+        color_num = color_num + 1;
+        color_num = color_num % 5;
+        cout << color_num << "." << endl;
+        break;
+    default:
+        cout << "Unhandled key press " << key << "." << endl;        
+    }
+
+	// this will refresh the screen so that the user sees the color change
+    glutPostRedisplay();
+}
+
+// This function is called whenever a "Special" key press is received.
+// Right now, it's handling the arrow keys.
+void specialFunc( int key, int x, int y )
+{
+    switch ( key )
+    {
+    case GLUT_KEY_UP:
+        // add code to change light position
+		// cout << "Unhandled key press: up arrow." << endl;
+        light_rp = light_rp + 0.5;
+		break;
+    case GLUT_KEY_DOWN:
+        // add code to change light position
+		// cout << "Unhandled key press: down arrow." << endl;
+        light_rp = light_rp - 0.5;
+		break;
+    case GLUT_KEY_LEFT:
+        // add code to change light position
+		// cout << "Unhandled key press: left arrow." << endl;
+        light_lp = light_lp - 0.5;
+		break;
+    case GLUT_KEY_RIGHT:
+        // add code to change light position
+		// cout << "Unhandled key press: right arrow." << endl;
+        light_lp = light_lp + 0.5;
+		break;
+    }
+
+	// this will refresh the screen so that the user sees the light position
+    glutPostRedisplay();
+}
+
+// This function is responsible for displaying the object.
+void drawScene(void)
+{
+    int i;
+
+    // Clear the rendering window
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Rotate the image
+    glMatrixMode( GL_MODELVIEW );  // Current matrix affects objects positions
+    glLoadIdentity();              // Initialize to the identity
+
+    // Position the camera at [0,0,5], looking at [0,0,0],
+    // with [0,1,0] as the up direction.
+    gluLookAt(0.0, 0.0, 5.0,
+              0.0, 0.0, 0.0,
+              0.0, 1.0, 0.0);
+
+    // Set material properties of object
+
+	// Here are some colors you might use - feel free to add more
+    GLfloat diffColors[5][4] = { {0.5, 0.5, 0.9, 1.0},
+                                 {0.9, 0.5, 0.5, 1.0},
+                                 {0.5, 0.9, 0.3, 1.0},
+                                 {0.3, 0.8, 0.9, 1.0},
+                                 {0.1, 0.3, 0.7, 1.0} };
+    
+    // cout << color_num << endl;
+
+	// Here we use the first color entry as the diffuse color
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffColors[color_num]);
+
+	// Define specular color and shininess
+    GLfloat specColor[] = {1.0, 1.0, 1.0, 1.0};
+    GLfloat shininess[] = {100.0};
+
+	// Note that the specular color and shininess can stay constant
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specColor);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+  
+    // Set light properties
+
+    // Light color (RGBA)
+    GLfloat Lt0diff[] = {1.0,1.0,1.0,1.0};
+    // Light position
+	//GLfloat Lt0pos[] = {1.0f, 1.0f, 5.0f, 1.0f};
+    GLfloat Lt0pos[] = {light_lp, light_rp, 5.0f, 1.0f };
+
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, Lt0diff);
+    glLightfv(GL_LIGHT0, GL_POSITION, Lt0pos);
+
+	// This GLUT method draws a teapot.  You should replace
+	// it with code which draws the object you loaded.
+	// glutSolidTeapot(1.0);
+    for (int k = 0; k < vecf.size(); k++) {
+        // a b c d e f g h i
+        vector<int> token = vecf[k];
+        int a = token[0];
+        //b = token[1];
+        int c = token[2];
+        int d = token[3];
+        //e = token[4];%
+        int f = token[5];
+        int g = token[6];
+        //h = token[7];
+        int i = token[8];
+        // cout << a << c << d << f << g << endl;
+        glBegin(GL_TRIANGLES);
+        glNormal3d(vecn[c - 1][0], vecn[c - 1][1], vecn[c - 1][2]);
+        glVertex3d(vecv[a - 1][0], vecv[a - 1][1], vecv[a - 1][2]);
+        glNormal3d(vecn[f - 1][0], vecn[f - 1][1], vecn[f - 1][2]);
+        glVertex3d(vecv[d - 1][0], vecv[d - 1][1], vecv[d - 1][2]);
+        glNormal3d(vecn[i - 1][0], vecn[i - 1][1], vecn[i - 1][2]);
+        glVertex3d(vecv[g - 1][0], vecv[g - 1][1], vecv[g - 1][2]);
+        glEnd();
+    }
+    
+    // Dump the image to the screen.
+    glutSwapBuffers();
+
+
+}
+
+// Initialize OpenGL's rendering modes
+void initRendering()
+{
+    glEnable(GL_DEPTH_TEST);   // Depth testing must be turned on
+    glEnable(GL_LIGHTING);     // Enable lighting calculations
+    glEnable(GL_LIGHT0);       // Turn on light #0.
+}
+
+// Called when the window is resized
+// w, h - width and height of the window in pixels.
+void reshapeFunc(int w, int h)
+{
+    // Always use the largest square viewport possible
+    if (w > h) {
+        glViewport((w - h) / 2, 0, h, h);
+    } else {
+        glViewport(0, (h - w) / 2, w, w);
+    }
+
+    // Set up a perspective view, with square aspect ratio
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // 50 degree fov, uniform aspect ratio, near = 1, far = 100
+    gluPerspective(50.0, 1.0, 1.0, 100.0);
+}
+
+void loadInput()
+{
+	// load the OBJ file here
+    // char buffer[MAXIMUM_REPARSE_DATA_BUFFER_SIZE];
+    // cin.getline(buffer, MAXIMUM_REPARSE_DATA_BUFFER_SIZE);
+
+    while (true)
+    {
+        char line[buffer_size];
+        if (!cin.getline(line, buffer_size)) break;
+        stringstream ss(line);
+        Vector3f v;
+        string s;
+        ss >> s;
+        if (s=="v") {
+            ss >> v[0] >> v[1] >> v[2];
+            vecv.push_back(v);
+        }
+        else if (s=="vn") {
+            ss >> v[0] >> v[1] >> v[2];
+            vecn.push_back(v);
+        }
+        else if (s == "f") {
+            // a/b/v d/e/f g/h/i
+            vector<int> f;
+            vector<string> f_strs(3);
+            ss >> f_strs[0] >> f_strs[1] >> f_strs[2];
+            for (int i = 0; i < 3; i++) {
+                char c[1024];
+                strcpy(c, f_strs[i].c_str());         
+                char* item = strtok(c, "/");
+                while (item) {
+                    f.push_back(atoi(item));               
+                    item = strtok(NULL, "/");
+                }
+            }
+            vecf.push_back(f);
+        }
+    }
+    
+}
+
+// Main routine.
+// Set up OpenGL, define the callbacks and start the main loop
+int main( int argc, char** argv )
+{
+
+    loadInput();
+
+    glutInit(&argc,argv);
+
+    // We're going to animate it, so double buffer 
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+
+    // Initial parameters for window position and size
+    glutInitWindowPosition( 60, 60 );
+    glutInitWindowSize( 360, 360 );
+    glutCreateWindow("Assignment 0");
+
+    // Initialize OpenGL parameters.
+    initRendering();
+
+    color_num = 0; // blue
+
+    // Set up callback functions for key presses
+    glutKeyboardFunc(keyboardFunc); // Handles "normal" ascii symbols
+    glutSpecialFunc(specialFunc);   // Handles "special" keyboard keys
+
+     // Set up the callback function for resizing windows
+    glutReshapeFunc( reshapeFunc );
+
+    // Call this whenever window needs redrawing
+    glutDisplayFunc( drawScene );
+
+    // Start the main loop.  glutMainLoop never returns.
+    glutMainLoop( );
+
+    return 0;	// This line is never reached.
+}
